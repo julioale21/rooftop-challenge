5842839f76907940c4a3348f03d6a7a33e2e37d3
Julio Romero <julio_ale21@hotmail.com>
2021-07-27 22:56:00 -0300

    refactor: changed logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Image, Nav, Navbar } from "react-bootstrap";
 import { LinkItem } from "../../components";
+import logo from "../../assets/logo.png";
 import "./styles.css";
 
 const Header: React.FC = () => {
   return (
     <Navbar expand="lg" id="header" variant="dark">
-      <Image className="ms-5" src="http://setuc.tech/images/logo_white.svg" width="100px" />
+      <Image className="ms-5" src={logo} width="100px" />
       <Navbar.Toggle aria-controls="navbarScroll" />
       <Navbar.Collapse id="navbarScroll">
         <Nav navbarScroll className="nav">
