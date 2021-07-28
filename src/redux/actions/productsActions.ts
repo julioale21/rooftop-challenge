84bd2db8918c0eced33a2ebbe43c2746ebe09353
Julio Romero <julio_ale21@hotmail.com>
2021-07-28 19:07:52 -0300

    added products action

---
+++ src/redux/actions/productsActions.ts
@@ -0,0 +1,12 @@
+import { FETCH_PRODUCTS } from "./actionsTypes";
+import { Dispatch } from "redux";
+import ProductsService from "../ProductsService";
+
+export const fetchPosts = (limit?: number) => async (dispatch: Dispatch) => {
+  ProductsService.fetchProducts(limit).then((products) => {
+    dispatch({
+      type: FETCH_PRODUCTS,
+      payload: { products },
+    });
+  });
+};
