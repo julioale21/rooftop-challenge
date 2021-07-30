d71a739ce47c8069537c0e55bf38bb8c7bf4b680
Julio Romero <julio_ale21@hotmail.com>
2021-07-29 23:45:24 -0300

    chore: exported default component dicount card

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Header } from "./Header";
 export { default as LinkItem } from "./LinkItem";
 export { default as ProductItem } from "./ProductItem";
 export { default as ProductList } from "./ProductList";
+export { default as DiscountCard } from "./DiscountCard";
