c3e1c6897dd6e7a7a112e027634deef483261380
Julio Romero <julio_ale21@hotmail.com>
2021-07-29 23:38:43 -0300

    chore: added footer to app layout

--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Header } from "../../components";
+import Footer from "../../components/Footer";
 import "./styles.css";
 
 const AppLayout: React.FC = ({ children }) => (
   <div id="app-layout">
     <Header />
     {children}
+    <Footer />
   </div>
 );
 
