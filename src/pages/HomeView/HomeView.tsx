6865de4e7179ee7de9eead102f40a8555268ad2a
Julio Romero <julio_ale21@hotmail.com>
2021-07-30 15:07:43 -0300

    feat(home): added user can view 4 products  randomly

--- src/pages/HomeView/HomeView.tsx
+++ src/pages/HomeView/HomeView.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CarouselImages, ProductList } from "../../components";
+import { CarouselImages, ProductList, ProductListItemSkeleton } from "../../components";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../redux/actions/productsActions";
 import banner1 from "../../assets/banner_1.jpg";
@@ -7,6 +7,7 @@ import banner2 from "../../assets/banner_2.jpg";
 import banner3 from "../../assets/banner_3.jpg";
 import IState from "../../interfaces/IEstate";
 import Product from "../../models/Product";
+import { shuffle } from "underscore";
 import "./styles.css";
 
 const HomeView = () => {
@@ -28,7 +29,11 @@ const HomeView = () => {
     <div>
       <CarouselImages images={images} />
       <h1 className="home-title">Recent Products</h1>
-      <ProductList products={products} />
+      {products.length ? (
+        <ProductList products={shuffle(products)} />
+      ) : (
+        <ProductListItemSkeleton quantity={4} />
+      )}
     </div>
   );
 };
