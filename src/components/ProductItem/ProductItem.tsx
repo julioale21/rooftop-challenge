8571464a88bbcd64e3ca9154fea90da24224ecf6
Julio Romero <julio_ale21@hotmail.com>
2021-07-30 14:40:29 -0300

    chore: updated product item design and styles

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Card, Col, Image } from "react-bootstrap";
-import { parseCurrency } from "../../utils/currency";
+import { getDiscount, parseCurrency } from "../../utils/currency";
+import { isCurrent } from "../../utils/days";
 import Product from "../../models/Product";
+import { DiscountCard } from "../../components";
 import "./styles.css";
 
 interface Props {
@@ -9,15 +11,25 @@ interface Props {
 }
 
 const ProductItem: React.FC<Props> = ({ product }) => {
+  const today = new Date();
+  const hasOffer: Boolean = product.offer !== null;
+
   return (
-    <Col lg={3} md={4} xs={11}>
+    <Col className="product-card-container" lg={3} md={4} xs={11}>
       <Card className="proudct-card">
-        <div className="top-item">
-          <h3>asdfasdfas</h3>
-        </div>
         <Image fluid src={product.images[0]} />
-        <h6 className="product-card-title">{product.title}</h6>
-        <p className="product-card-price">{parseCurrency(Number(product.price))}</p>
+        <div className="product-card-body">
+          <h6 className="product-card-title">{product.title}</h6>
+          <p className={`product-card-price ${hasOffer ? "offer" : ""}`}>
+            {parseCurrency(Number(product.price))}
+          </p>
+        </div>
+        {hasOffer && isCurrent(today.toString(), product.offer.expires_at) ? (
+          <DiscountCard
+            discount={product.offer.price}
+            off={getDiscount(Number(product.price), Number(product.offer.price))}
+          />
+        ) : null}
         <div className="bottom-item">
           <button>Add to cart</button>
         </div>
