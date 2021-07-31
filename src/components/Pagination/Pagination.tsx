7c095fd5b6c0670e17c77df6fc6da18aa2d02f0d
Julio Romero <julio_ale21@hotmail.com>
2021-07-31 10:28:12 -0300

    styles: added styles to pagination item

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,13 +1,30 @@
 import React from "react";
+import "./styles.css";
 
 interface Props {
-  page: number;
+  page?: number;
   pageCount: number;
   handlePrev: () => void;
   handleNext: () => void;
 }
-const Pagination: React.FC<Props> = () => {
-  return <div></div>;
+const Pagination: React.FC<Props> = ({ page = 0, pageCount, handleNext, handlePrev }) => {
+  return (
+    <div className="d-flex justify-content-center mb-5">
+      {page > 0 && pageCount >= page ? (
+        <button className="pagination-button" onClick={handlePrev}>
+          Prev
+        </button>
+      ) : null}
+      <div className="page">
+        <p>{page}</p>
+      </div>
+      {page < pageCount - 1 ? (
+        <button className="pagination-button" onClick={handleNext}>
+          Next
+        </button>
+      ) : null}
+    </div>
+  );
 };
 
 export default Pagination;
