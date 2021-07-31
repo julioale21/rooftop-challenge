613d1d3839b3498f5c14ada42016529a8a79dd57
Julio Romero <julio_ale21@hotmail.com>
2021-07-31 19:52:23 -0300

    chore: updated utils days

--- src/utils/days.ts
+++ src/utils/days.ts
@@ -1,9 +1,9 @@
 import dayjs from "dayjs";
 
-export const isCurrent = (currentDate: string, expiresDate: string) => {
+export const hasExpired = (currentDate: string, expiresDate: string) => {
   const date1 = dayjs(currentDate);
   const date2 = dayjs(expiresDate);
   const difference = date2.diff(date1);
 
-  return difference > 0;
+  return difference < 0;
 };
