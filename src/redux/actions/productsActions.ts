7da6ae2e937df30b9755b179a3cb418f07f3baed
Julio Romero <julio_ale21@hotmail.com>
2021-07-31 17:36:14 -0300

    chore: added action to update is loading attribute

--- src/redux/actions/productsActions.ts
+++ src/redux/actions/productsActions.ts
@@ -1,9 +1,20 @@
-import { FETCH_PRODUCTS } from "./actionsTypes";
+import { FETCH_PRODUCTS, SET_ISLOADING } from "./actionsTypes";
 import { Dispatch } from "redux";
 import ProductsService from "../ProductsService";
 
+const setIsLoading = (isLoading: boolean, dispatch: Dispatch) => {
+  dispatch({
+    type: SET_ISLOADING,
+    payload: {
+      isLoading,
+    },
+  });
+};
+
 export const fetchProducts = (limit?: number) => async (dispatch: Dispatch) => {
+  setIsLoading(true, dispatch);
   ProductsService.fetchProducts(limit).then((products) => {
+    setIsLoading(false, dispatch);
     dispatch({
       type: FETCH_PRODUCTS,
       payload: { products },
