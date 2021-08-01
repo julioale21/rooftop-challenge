f8fa0525584ddeb89527189c91f7c8ed1bf3616b
Julio Romero <julio_ale21@hotmail.com>
2021-07-31 22:22:28 -0300

    chore: extracted carousel images to utils

---
+++ src/utils/carouselImages.ts
@@ -0,0 +1,9 @@
+import banner1 from "../assets/banner_1.jpg";
+import banner2 from "../assets/banner_2.jpg";
+import banner3 from "../assets/banner_3.jpg";
+
+export const images = [
+  { name: "banner1", url: banner1 },
+  { name: "banner2", url: banner2 },
+  { name: "banner3", url: banner3 },
+];
