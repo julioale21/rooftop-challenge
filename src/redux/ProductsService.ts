6b942cc46861f00ec593e74e20c1d41096902a28
Julio Romero <julioale21@gmail.com>
2021-08-03 17:19:30 -0300

    chore: added send question action on service

--- src/redux/ProductsService.ts
+++ src/redux/ProductsService.ts
@@ -2,22 +2,36 @@ import axios from "axios";
 import Product from "../models/Product";
 import Question from "../models/Question";
 
-const baseURL = "https://rooftop-api-rest-frontend.herokuapp.com/";
+const baseURL = "https://rooftop-api-rest-frontend.herokuapp.com";
 
 class ProductsService {
-  static async fetchProducts(limit?: number) {
-    const response = await axios.get(`${baseURL}items${limit ? "?limit=" + limit : ""}`);
+  static async fetchProducts(limit?: number): Promise<Product[]> {
+    const response = await axios.get(`${baseURL}/items${limit ? "?limit=" + limit : ""}`);
     const products: Product[] = response.data.items;
 
     return products;
   }
 
-  static async fetchQuestions(product_id: number) {
-    const response = await axios.get(`${baseURL}questions?item_id=${product_id}`);
+  static async fetchQuestions(product_id: number): Promise<Question[]> {
+    const response = await axios.get(`${baseURL}/questions?item_id=${product_id}`);
 
     const questions: Question[] = response.data;
 
     return questions;
+  }
+
+  static async sendQuestion(question: Question): Promise<number> {
+    const response = await axios.post(
+      `${baseURL}/questions`,
+      { ...question },
+      {
+        headers: {
+          "Content-Type": "application-json",
+        },
+      },
+    );
+
+    return response.status;
   }
 }
 
