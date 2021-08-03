f7ac04af3569e5ed7567400dd50821b15f1be0aa
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-08-03 10:41:46 -0300

    chore: added question item compoent

---
+++ src/components/Question/QuestionItem/QuestionItem.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Question from "../../../models/Question";
+import { getTimeFromTimeStamp } from "../../../utils/days";
+import "./styles.css";
+
+interface Props {
+  question: Question;
+}
+
+const QuestionItem: React.FC<Props> = ({ question }) => {
+  return (
+    <div className="question-container">
+      <h6 className="question-customer">{question.customer_name}</h6>
+      <p className="question">{question.question}</p>
+      <p className="answer">{question.answer}</p>
+      <p className="question-date">{getTimeFromTimeStamp(question.sent_at)}</p>
+    </div>
+  );
+};
+
+export default QuestionItem;
