f7abc902b77fb4fdcd807f791f4859be5486ffb3
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-08-03 12:46:26 -0300

    chore: added formik to question form and added styles

--- src/components/Question/QuestionForm/QuestionForm.tsx
+++ src/components/Question/QuestionForm/QuestionForm.tsx
@@ -1,13 +1,54 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, FormGroup, FormControl } from "react-bootstrap";
+import { useFormik } from "formik";
+import { schema } from "./validationSchema";
+import { initialValues } from "./initialValues";
+import { FormError } from "../../../components";
+import "./styles.css";
 
 const QuestionForm = () => {
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: schema,
+    onSubmit: (values) => {
+      // eslint-disable-next-line no-console
+      console.log(values);
+    },
+  });
+
   return (
     <Row className="mt-5">
       <Col className="mx-auto" md={6} xs={12}>
-        <form>
-          <input type="text" />
-          <button>Send</button>
+        <form onSubmit={formik.handleSubmit}>
+          <FormGroup className="mt-4">
+            <FormControl
+              name="username"
+              placeholder="Enter username"
+              type="text"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+            />
+            {formik.touched.username && formik.errors.username ? (
+              <FormError errorText={formik.errors.username} />
+            ) : null}
+          </FormGroup>
+
+          <FormGroup className="mt-4">
+            <FormControl
+              as="textarea"
+              name="message"
+              placeholder="Enter your message"
+              value={formik.values.message}
+              onChange={formik.handleChange}
+            />
+            {formik.touched.message && formik.errors.message ? (
+              <FormError errorText={formik.errors.message} />
+            ) : null}
+          </FormGroup>
+
+          <button className="send-button" type="submit">
+            Submit
+          </button>
         </form>
       </Col>
     </Row>
