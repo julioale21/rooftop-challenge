ff25a9d3faa78794f78712190cf94e8ecfee97f6
Julio Romero <julioale21@gmail.com>
2021-08-03 17:24:46 -0300

    chore: added and configured react toast notification on main

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App";
+import { ToastProvider } from "react-toast-notifications";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <ToastProvider autoDismiss autoDismissTimeout={2000} placement="bottom-center">
+        <App />
+      </ToastProvider>
     </React.StrictMode>
   </Provider>,
   document.getElementById("root"),
