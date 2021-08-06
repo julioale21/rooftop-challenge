3d4171697bcab2cf949df4a40bc14cc9e43a8dfd
Julio Romero <julioale21@gmail.com>
2021-08-05 22:36:16 -0300

    chore: added redirect to home when user click logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Image, Nav, Navbar } from "react-bootstrap";
 import { LinkItem } from "../../components";
+import { useHistory } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import "./styles.css";
 
 const Header: React.FC = () => {
+  const history = useHistory();
+  const handleClick = () => {
+    history.push("/");
+  };
+
   return (
     <Navbar expand="md" id="header" variant="light">
-      <Image className="ms-5" src={logo} width="100px" />
+      <Image className="ms-5" src={logo} width="100px" onClick={handleClick} />
       <Navbar.Toggle aria-controls="navbarScroll" />
       <Navbar.Collapse id="navbarScroll">
         <Nav navbarScroll className="nav">
